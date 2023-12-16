857a87ea90bfd3253efc53db1ad839320fce57aa
marianickolaevna <savannawriter@gmail.com>
2023-12-16 21:44:40 +0300

    first and last

--- src/components/Recomendations.tsx
+++ src/components/Recomendations.tsx
@@ -10,10 +10,12 @@ interface Props {
 const Recomendations = ({ index }: Props) => {
   const minus = meals[index - 1];
   const plus = meals[index + 1];
+  const last: any = meals[meals.length - 1];
+  const first: any = meals[0];
 
   return (
     <div className="rec-wrapper">
-      {minus && (
+      {minus ? (
         <Link
           to="/meal"
           state={{
@@ -29,8 +31,24 @@ const Recomendations = ({ index }: Props) => {
             <img className="rec-img" src={minus.image} />
           </div>
         </Link>
+      ) : (
+        <Link
+          to="/meal"
+          state={{
+            image: last.image,
+            title: last.title,
+            description: last.description,
+            index: meals.length - 1,
+          }}
+          style={{ textDecoration: "none" }}
+        >
+          <div className="rec-item">
+            <img className="arrow" src={arrow} />
+            <img className="rec-img" src={last.image} />
+          </div>
+        </Link>
       )}
-      {index < meals.length - 1 && plus && (
+      {index < meals.length - 1 && plus ? (
         <Link
           to="/meal"
           state={{
@@ -44,6 +62,23 @@ const Recomendations = ({ index }: Props) => {
         >
           <div className="rec-item">
             <img className="rec-img" src={plus.image} />
+            <img className="arrow right" src={arrow} />
+          </div>
+        </Link>
+      ) : (
+        <Link
+          to="/meal"
+          state={{
+            image: first.image,
+            title: first.title,
+            description: first.description,
+            index: 0,
+          }}
+          style={{ textDecoration: "none" }}
+          preventScrollReset={false}
+        >
+          <div className="rec-item">
+            <img className="rec-img" src={first.image} />
             <img className="arrow right" src={arrow} />
           </div>
         </Link>
