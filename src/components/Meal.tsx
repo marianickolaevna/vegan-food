3e205b1e119000c1918515eba11d8687e5551bcc
marianickolaevna <savannawriter@gmail.com>
2023-12-16 18:52:15 +0300

    more css

--- src/components/Meal.tsx
+++ src/components/Meal.tsx
@@ -20,10 +20,7 @@ const Meal = () => {
         </div>
       </Link>
       <div className="d-flex flex-column align-items-center justify-content-center">
-        <div
-          className="d-flex flex-column align-items-center justify-content-between"
-          style={{ maxWidth: "75%", marginTop: "10%", marginBottom: "15%" }}
-        >
+        <div className="d-flex flex-column align-items-center justify-content-between meal-wrapper">
           <div className="h1 heading meal-heading">{state.title}</div>
           <img src={state.image} alt="..." className="meal-image" />
           <div className="meal-des">{state.description}</div>
