46aa14932d43ff9bccb9c0c90ff709bdfa81a37f
marianickolaevna <savannawriter@gmail.com>
2023-12-20 16:29:35 +0300

    commit

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -8,16 +8,7 @@ interface Props {
 const Step = ({ text, num }: Props) => {
   return (
     <>
-      <div
-        style={{
-          fontSize: "2vw",
-          fontWeight: "500",
-          margin: "1vw",
-          marginTop: "4vw",
-        }}
-      >
-        {"Шаг " + num}
-      </div>
+      <div className="step">{"Шаг " + num}</div>
       <div className="meal-des">{text}</div>
     </>
   );
--- src/components/muiB.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Button from "@mui/material/Button";
-
-const Mui = () => {
-  return <Button variant="contained">Hello world</Button>;
-};
-
-export default Mui;
